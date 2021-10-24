8b7f97aae2f78ddbbc9cdb1045238a5598be36a9
debabratabanerjee <47970328+debabratabanerjee@users.noreply.github.com>
2021-10-24 08:57:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import JSONDATA from './club.json';
+
+export default function App() {
+  return (
+    <div className="App">
+      <input type= "text" placeholder="search the cadidate..."></input>
+    </div>
+  );
+}
